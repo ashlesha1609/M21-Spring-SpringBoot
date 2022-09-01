1bd9c533102c060b08951bb454fa1103f5a721bc
HP <HP@LAPTOP-QL7VGDC3>
2022-09-01 13:30:47 +0530

    First Commit

---
+++ SpringDependencyInjection/src/main/java/com/tns/di/Exam.java
@@ -0,0 +1,16 @@
+package com.tns.di;
+
+public class Exam
+{
+	public static void main(String[] args)
+	{
+		Students a=new Students();
+		a.display();
+		
+		
+		
+		
+
+	}
+
+}
---
+++ SpringDependencyInjection/src/main/java/com/tns/di/Students.java
@@ -0,0 +1,11 @@
+package com.tns.di;
+
+public class Students 
+{
+	String StudentName;
+	public void display()
+	{
+		System.out.println("Name of the Students is: "+StudentName);
+	}
+
+}
